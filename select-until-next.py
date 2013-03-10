65bac1aa18d2dc3983c4a197efee30b6965bb19b
Xavi <xavi.rmz@gmail.com>
2013-03-10 14:34:31 -0700

    Improved functionality and fixed bugs.

--- select-until-next.py
+++ select-until-next.py
@@ -2,11 +2,19 @@ import sublime, sublime_plugin
 
 import re
 
-def parse_input(view, input):
+def on_done(view):
+	newSels = view.get_regions("select-until")
+	view.erase_regions("select-until")
+
 	sels = view.sel()
+	sels.clear()
+	for sel in newSels:
+		sels.add(sel)
+
+def on_change(view, oriSels, text):
 	newSels = []
-	for sel in sels:
-		regFind = view.find(re.escape(input), sel.begin())
+	for sel in oriSels:
+		regFind = view.find(re.escape(text), sel.begin())
 
 		if regFind is None: continue
 
@@ -14,19 +22,25 @@ def parse_input(view, input):
 		newSels.append(regExpand)
 
 	view.add_regions("select-until", newSels, "comment", "", sublime.DRAW_OUTLINED)
+
+def on_cancel(view, oriSels):
+	view.erase_regions("select-until")
+
+	sels = view.sel()
 	sels.clear()
-	for sel in newSels:
+	for sel in oriSels:
 		sels.add(sel)
 
 class SelectUntilNextCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit):
 		view = self.view
+		oriSels = [sel for sel in view.sel()]
 
 		view.window().show_input_panel(
 			"Select Until Next -- [count] or {chars} or /regex/",
 			"",
-			lambda input: 0,
-			lambda input: parse_input(view, input),
-			lambda : 0
+			lambda text: on_done(view),
+			lambda text: on_change(view, oriSels, text),
+			lambda : on_cancel(view, oriSels)
 		)
